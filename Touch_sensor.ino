eb3e3f97f994170e3d017176754e3516a9ed7243
AkshayNachappa <46316452+AkshayNachappa@users.noreply.github.com>
2019-03-16 23:18:12 +0530

    Update Touch_sensor.ino

--- Touch_sensor.ino
+++ Touch_sensor.ino
@@ -10,7 +10,7 @@ void loop()
   touch_value=touchRead(32);//read touch value from pin 32
   Serial.println(touch_value);
 
-   if (touch_value<50)
+   if (touch_value<50) // calibrated value. Calibrate your own value by trial and error
    {
     digitalWrite(2,HIGH);
    }
